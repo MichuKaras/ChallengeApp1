7c799f99d23e9cd7a3fb14b19f123ddd92e4e646
MichuKaras <mkaras.karas6@gmail.com>
2023-02-08 19:06:07 +0100

    Zadanie dzień 6

---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,33 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string name, string surename, string age)
+        {
+            this.Name = name;
+            this.Surename = surename;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public string Surename { get; private set; }
+
+        public string Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
