f322e9bf02beb51f2563341c981827eb564c3d5d
MichuKaras <mkaras.karas6@gmail.com>
2023-02-16 10:17:10 +0100

    Zadanie dzień 9

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,57 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void TestForMinStats()
+        {
+
+            // arrange
+            var employee = new Employee("Michał", "Karaś");
+            
+            employee.AddGrade(6);
+            employee.AddGrade(7);
+            employee.AddGrade(4);
+            employee.AddGrade(5);
+            employee.AddGrade(1);
+
+            // act
+            var statistics = employee.GetStatistics();
+
+            // assert
+            Assert.AreEqual(1, statistics.Min);
+        }
+
+        [Test]
+        public void TestForMaxStats()
+        {
+            var employee = new Employee("Michał", "Karaś");
+
+            employee.AddGrade(7);
+            employee.AddGrade(3);
+            employee.AddGrade(2);
+            employee.AddGrade(6);
+            employee.AddGrade(3);
+
+            var statistics = employee.GetStatistics();
+
+            Assert.AreEqual(7, statistics.Max);
+        }
+
+        [Test]
+        public void TestForAverageStats()
+        {
+            var employee = new Employee("Michał", "Karaś");
+
+            employee.AddGrade(9);
+            employee.AddGrade(6);    
+            employee.AddGrade(4);
+            employee.AddGrade(6);
+            employee.AddGrade(2);
+
+            var statistics = employee.GetStatistics();
+
+            Assert.AreEqual(5.4f, statistics.Average);
+        }
+    }
+}
---
+++ ChallengeApp/ChallengeApp/ChallengeApp.cs
@@ -0,0 +1,13 @@
+﻿using ChallengeApp;
+
+var employee = new Employee("Michał", "Karaś");
+
+employee.AddGrade(6);
+employee.AddGrade(2);
+employee.AddGrade(2);
+
+var statistics = employee.GetStatistics();
+
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min}");
+Console.WriteLine($"Max: {statistics.Max}");
